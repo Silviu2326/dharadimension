2653555d66fb85067f74b85e7877c95bc1bb91f0
Your Name <Silviu2326@users.noreply.github.com>
2026-02-05 12:14:37 +0100

    hola

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.tsx';
 import Unsubscribe from './Unsubscribe.tsx';
 import './index.css';
@@ -11,6 +11,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/unsubscribe" element={<Unsubscribe />} />
+        {/* Ruta alternativa por si hay typo */}
+        <Route path="/unsuscribe" element={<Navigate to="/unsubscribe" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
